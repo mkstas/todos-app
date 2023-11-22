621f668d4ba218d17d29fb5ffc1808c49b8bfddd
mkstas <makarov.stn@gmail.com>
2023-11-22 23:22:52 +0300

    refactor: new favicon, icon logo

--- src/shared/uilib/BaseLogotype/ui/BaseLogotype.vue
+++ src/shared/uilib/BaseLogotype/ui/BaseLogotype.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { CheckBadgeIcon } from '@heroicons/vue/24/outline';
+import { CheckCircleIcon } from '@heroicons/vue/24/outline';
 </script>
 
 <template>
   <div
     class="flex items-center justify-center gap-2 drop-shadow-md shadow-gray-800/15"
   >
-    <CheckBadgeIcon class="h-10 text-sky-500" />
+    <CheckCircleIcon class="h-10 text-sky-500" />
     <span class="text-2xl font-semibold">Task Tracker</span>
   </div>
 </template>
