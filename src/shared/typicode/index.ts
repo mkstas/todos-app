97991a0c8e452d94b4fc05b3fc6da63b8ef0a26b
mkstas <makarov.stn@gmail.com>
2023-11-28 21:29:33 +0300

    refactor: rename entities

--- src/shared/typicode/index.ts
+++ src/shared/typicode/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/routes.types';
-export * from './lib/section.types';
 export * from './lib/task.types';
+export * from './lib/taskGroup.types';
 export * from './lib/user.types';
