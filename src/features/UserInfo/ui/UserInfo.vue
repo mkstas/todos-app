d51b2cfd5fb85a68814618e7085e012c771e0a40
mkstas <makarov.stn@gmail.com>
2023-12-01 16:53:56 +0300

    dev: create feature UserInfo

---
+++ src/features/UserInfo/ui/UserInfo.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { BaseHeading, BaseHeadingEnum } from '@/shared/uilib';
+
+import { useUserStore } from '@/entities/user';
+
+const { user } = useUserStore();
+</script>
+
+<template>
+  <div>
+    <BaseHeading :type="BaseHeadingEnum.h3">{{ user.userName }}</BaseHeading>
+    <p class="text-base text-gray-500">{{ user.email }}</p>
+  </div>
+</template>
--- src/widgets/TheSidebar/ui/TheSidebar.vue
+++ src/widgets/TheSidebar/ui/TheSidebar.vue
@@ -3,24 +3,21 @@ import { BaseButton, BaseLogotype } from '@/shared/uilib';
 
 import { useUserStore } from '@/entities/user';
 
+import { UserInfo } from '@/features/UserInfo';
 import { TaskGroup } from '@/features/TaskGroup';
 
-const { user, signOutWithRedirect } = useUserStore();
+const { signOutWithRedirect } = useUserStore();
 </script>
 
 <template>
-  <div
+  <section
     class="grid md:grid-rows-[auto_1fr_auto] gap-6 p-3 bg-gray-100 max-md:border-b md:border-r border-slate-200"
   >
     <BaseLogotype />
     <div class="flex-1 flex flex-col gap-8">
-      <div>
-        <p class="text-xl font-semibold">{{ user.userName }}</p>
-        <p class="text-base text-gray-500">{{ user.email }}</p>
-      </div>
+      <UserInfo />
       <TaskGroup />
     </div>
     <BaseButton @click="signOutWithRedirect">Выйти</BaseButton>
-  </div>
+  </section>
 </template>
-@/features/TaskGroup
