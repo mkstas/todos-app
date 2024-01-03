c10800c6920a2f8c4b4c9f829163dd1bb3ab6be1
mkstas <makarov.stn@gmail.com>
2024-01-03 23:15:55 +0300

    dev: craete ui BaseTextarea

---
+++ src/shared/uilib/BaseTextarea/BaseTextarea.types.ts
@@ -0,0 +1,8 @@
+export type BaseTextareaPropsType = {
+  modelValue: string;
+  label?: string;
+  id?: string;
+  placeholder?: string;
+  required?: boolean;
+  rows?: number;
+};
---
+++ src/shared/uilib/BaseTextarea/index.ts
@@ -0,0 +1,2 @@
+export { default as BaseTextarea } from './BaseTextarea.vue';
+export * from './BaseTextarea.types';
