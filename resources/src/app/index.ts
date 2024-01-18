058eae0a12fd1320fd5c539894469764d087013d
mkstas <makarov.stn@gmail.com>
2024-01-18 21:50:27 +0300

    feat: install vuetify, axios

--- resources/src/app/index.ts
+++ resources/src/app/index.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 
-import { router, store } from './providers';
+import { router, store, vuetify } from './providers';
 import App from './index.vue';
 
 export const bootstrap = () => {
-  createApp(App).use(store).use(router).mount('#app');
+  createApp(App).use(store).use(router).use(vuetify).mount('#app');
 };
