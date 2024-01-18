37981f1f3bfbf57bceba897136ae6d50589c7abc
mkstas <makarov.stn@gmail.com>
2024-01-19 02:26:38 +0300

    feat: create pages welcome, register, login

--- resources/src/app/providers/router/index.ts
+++ resources/src/app/providers/router/index.ts
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { RoutesNameEnum, RoutesPathEnum } from '@/shared/types';
+
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: RoutesPathEnum.welcome,
+      name: RoutesNameEnum.welcome,
+      component: () => import('@/pages/welcome-page'),
+    },
+    {
+      path: RoutesPathEnum.register,
+      name: RoutesNameEnum.register,
+      component: () => import('@/pages/register-page'),
+    },
+    {
+      path: RoutesPathEnum.login,
+      name: RoutesNameEnum.login,
+      component: () => import('@/pages/login-page'),
+    },
+  ],
 });
---
+++ resources/src/shared/types/lib/routes.types.ts
@@ -0,0 +1,11 @@
+export enum RoutesPathEnum {
+  welcome = '/',
+  register = '/register',
+  login = '/login',
+}
+
+export enum RoutesNameEnum {
+  welcome = 'Welcome',
+  register = 'Register',
+  login = 'Login',
+}
