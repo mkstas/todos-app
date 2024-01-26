01ac7b7cc7205ff7338a50728d954881fb618672
mkstas <makarov.stn@gmail.com>
2024-01-26 06:17:23 +0300

    feat: check empty url hash

--- src/pages/board-page/BoardPage.vue
+++ src/pages/board-page/BoardPage.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
+import { Ref, ref } from 'vue';
+import { ClipboardDocumentListIcon } from '@heroicons/vue/24/outline';
 import { VSheet } from '@/shared/ui';
 import { TaskForm, Sidebar, TaskList } from '@/widgets';
+
+const hash: Ref<string> = ref('');
+
+window.addEventListener('hashchange', () => {
+  hash.value = window.location.hash.slice(1);
+});
 </script>
 
 <template>
   <VSheet class="w-full max-w-5xl h-[32rem] grid grid-cols-[12rem_1fr]">
     <Sidebar />
     <main>
-      <TaskForm />
-      <TaskList />
+      <component v-if="hash">
+        <TaskForm />
+        <TaskList />
+      </component>
+      <div v-else class="h-full flex items-center justify-center">
+        <ClipboardDocumentListIcon class="w-32 h-32 text-zinc-300" />
+      </div>
     </main>
   </VSheet>
 </template>
