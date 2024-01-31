58389f279675df04232974ee261d464e98a3eed3
mkstas <makarov.stn@gmail.com>
2024-01-31 11:22:51 +0300

    feat: refactor page board

--- src/views/view-board/ViewBoard.vue
+++ src/views/view-board/ViewBoard.vue
@@ -1,69 +1,13 @@
 <script setup lang="ts">
 import { Ref, onMounted, provide, ref } from 'vue';
-import { IGroup, ITask } from '@/types';
 import { TheSidebar } from '@/components/layout/the-sidebar';
 import { TheContent } from '@/components/layout/the-content';
+import { useGroup, useTask } from '@/composables';
 
-const groups: Ref<IGroup[]> = ref([]);
-const tasks: Ref<ITask[]> = ref([]);
 const urlHash: Ref<string> = ref(window.location.hash.slice(1));
 
-const getGroups = () => {
-  if (localStorage.getItem('groups')) {
-    groups.value = JSON.parse(localStorage.getItem('groups') as string);
-  } else {
-    localStorage.setItem('groups', JSON.stringify([]));
-  }
-};
-
-const storeGroup = (title: string) => {
-  groups.value.push({ id: Date.now().toString(), title });
-  localStorage.setItem('groups', JSON.stringify(groups.value));
-};
-
-const getTasks = (groupId: string) => {
-  if (localStorage.getItem('tasks')) {
-    const savedTasks = JSON.parse(localStorage.getItem('tasks') as string);
-    const filteredTasks = savedTasks.filter(task => task.groupId === groupId);
-    tasks.value = filteredTasks;
-  } else {
-    localStorage.setItem('tasks', JSON.stringify([]));
-  }
-};
-
-const storeTask = (groupId: string, text: string) => {
-  tasks.value.push({ id: Date.now().toString(), groupId, text, status: false });
-  const savedTasks = JSON.parse(localStorage.getItem('tasks') as string);
-  savedTasks.push({
-    id: Date.now().toString(),
-    groupId,
-    text,
-    status: false,
-  });
-  localStorage.setItem('tasks', JSON.stringify(savedTasks));
-};
-
-const updateTask = (id: string, ...values: Array<{}>) => {
-  const savedTasks = JSON.parse(localStorage.getItem('tasks') as string);
-  for (let i = 0; i < savedTasks.length; i++) {
-    if (savedTasks[i].id === id) {
-      Object.assign(savedTasks[i], values[0]);
-    }
-  }
-  for (let i = 0; i < tasks.value.length; i++) {
-    if (tasks.value[i].id === id) {
-      Object.assign(tasks.value[i], values[0]);
-    }
-  }
-  localStorage.setItem('tasks', JSON.stringify(savedTasks));
-};
-
-const deleteTask = (id: string) => {
-  tasks.value = tasks.value.filter(task => task.id !== id);
-  const savedTasks = JSON.parse(localStorage.getItem('tasks') as string);
-  const updatedTasks = savedTasks.filter(task => task.id !== id);
-  localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-};
+const { groups, getGroups, storeGroup } = useGroup();
+const { tasks, getTasks, storeTask, updateTask, deleteTask } = useTask();
 
 provide('groups', groups);
 provide('tasks', tasks);
@@ -81,7 +25,6 @@ onMounted(() => {
   if (!isUrlHash) {
     urlHash.value = '';
   }
-
   window.addEventListener('hashchange', () => {
     urlHash.value = window.location.hash.slice(1);
     const isUrlHash = groups.value.find(group => group.id === urlHash.value);
